19e72dedf3e4bdbfb66cea95a277af15457291ce
brianleeanderson19 <brianleeanderson19@yahoo.com>
2017-01-11 14:23:43 -0700

    Updated status for enum

---
+++ app/controllers/GOT.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name_1 = gets.chomp
+
+puts "Hello, #{name_1}"
+
+if name_1 == ("Jon", "Arya", "jaqen h'ghar")
+    return "Hello #{name_1}"
+
+else
+    return "Winter is Coming
+end"
