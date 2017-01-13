45894ffdf2aff631774112854abd6486ec44c958
brianleeanderson19 <brianleeanderson19@yahoo.com>
2017-01-13 10:05:49 -0700

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 3.times do |topic|
    Topic.create!(
-      title: "#{topic}"
+      title: "Topic #{topic}"
       )
+end
+
+puts "3 topics created"
 
 10.times do |blog|
  Blog.create!(
    title: "My Blog Post #{blog}",
    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
-
    topic_id: Topic.last.id
 
    )
