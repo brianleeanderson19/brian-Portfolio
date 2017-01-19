d981beeb343295366d4634c53295a3327f894d2d
brianleeanderson19 <brianleeanderson19@yahoo.com>
2017-01-19 14:55:21 -0700

    Added gust user for current_user

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 
 module DevcampPortfolio
   class Application < Rails::Application
-    config.action_controller.permit_all_perameters = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
