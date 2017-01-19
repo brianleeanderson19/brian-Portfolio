322c1ba19f1aff8b6e6a5bcf66c3ddda8960bb72
brianleeanderson19 <brianleeanderson19@yahoo.com>
2017-01-19 16:11:05 -0700

    Added nav bar, and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,11 @@
+module DefaultPageContent
+    extend ActiveSupport::Concern
+    included do 
+        before_filter :set_page_defaults
+    end
+
+    def set_page_defaults
+        @page_title = "Devcamp Portfolio | My Portfolio Website"
+        @seo_keywords = "Brian Andersons portfolio"
+    end
+end
