49d913fa88219e9e46f08b5fa753db65a6bc2667
brianleeanderson19 <brianleeanderson19@yahoo.com>
2017-03-09 17:32:45 -0700

    title name

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -5,7 +5,7 @@ module DefaultPageContent
     end
 
     def set_page_defaults
-        @page_title = "Devcamp Portfolio | My Portfolio Website"
+        @page_title = "Anderson Portfolio | My Portfolio Website"
         @seo_keywords = "Brian Andersons portfolio"
     end
 end
